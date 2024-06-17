cc64f11e771dd64450848816dc2e742fce536b29
Evgeniya363 <evavong363@gmail.ru>
2024-06-17 19:34:23 +0300

    Dобавляем домашнюю работу к 2-му семинару

---
+++ lesson2/src/main/java/ru/gb/homework2/RandomAnnotationProcessor.java
@@ -0,0 +1,74 @@
+package ru.gb.homework2;
+
+import java.lang.reflect.Field;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+public class RandomAnnotationProcessor {
+    static java.util.Random random = new java.util.Random();
+
+    public static <T> void processAnnotation(T obj) {
+        // найти все поля класса, над которыми стоит аннотация @Random или @RandomDate
+        // вставить туда рандомное число в зависимости от типа данных поля
+        Class<?> objClass = obj.getClass();
+        for (Field field : objClass.getDeclaredFields()) {
+            if (field.isAnnotationPresent(Random.class)) {
+                setIntegerValue(obj, field);
+            } else if (field.isAnnotationPresent(RandomDate.class)) {
+                setDateValue(obj, field);
+            }
+
+        }
+
+    }
+
+    private static void setDateValue(Object obj, Field field) {
+        Class<?> type = field.getType();
+        RandomDate anno = field.getAnnotation(RandomDate.class);
+        long min = anno.min();
+        long max = anno.max();
+        String zone = anno.zone();
+        long result = random.nextLong(min, max);
+
+        try {
+            field.setAccessible(true); //Разрешаем редактирование приватных полей
+
+            if (type.isAssignableFrom(Date.class)) {
+                field.set(obj, new Date(result));
+            } else {
+                Instant instant = Instant.ofEpochMilli(result);
+
+                if (type.isAssignableFrom(Instant.class)) {
+                    field.set(obj, instant);
+                } else if (type.isAssignableFrom(LocalDate.class)) {
+                    field.set(obj, instant.atZone(ZoneId.of(zone)).toLocalDate());
+                } else if (type.isAssignableFrom(LocalDateTime.class)) {
+                    field.set(obj, instant.atZone(ZoneId.of(zone)).toLocalDateTime());
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void setIntegerValue(Object obj, Field field) {
+        Class<?> type = field.getType();
+        Random anno = field.getAnnotation(Random.class);
+        int min = anno.min();
+        int max = anno.max();
+        long result = random.nextLong(min, max);
+
+        try {
+            field.setAccessible(true);
+            if (type.isAssignableFrom(int.class) || type.isAssignableFrom(Integer.class)) {
+                field.set(obj, (int) result);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
