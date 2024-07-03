d84bd38198a9a6d275255e062e69a10568feac9c
Evgeniya363 <evavong363@gmail.ru>
2024-07-03 12:19:57 +0300

    добавляем работу к 4-му семинару

--- lesson4/src/main/java/ru/gb/post/PostComment.java
+++ lesson4/src/main/java/ru/gb/post/PostComment.java
@@ -1,24 +1,76 @@
 package ru.gb.post;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
-@NoArgsConstructor
+import java.time.LocalDate;
+
+import static java.time.LocalDate.now;
+
 @Entity
 @Table(name = "post_comment")
 public class PostComment {
     @Id
     @Column(name = "id")
-    long id;
+    private long id;
 
-    @Column(name = "post")
-    Post post;
+    @Column(name="timestamp")
+    LocalDate timestamp;
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+
+    @ManyToOne
+    @JoinColumn(name="user_id", nullable = false)
+    private MyUser myUser;
 
     @Column(name = "comment")
     String comment;
+
+    public PostComment() {
+        timestamp = now();
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Post getPost() {
+        return post;
+    }
+
+    public void setPost(Post post) {
+        this.post = post;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public MyUser getMyUser() {
+        return myUser;
+    }
+
+    public void setMyUser(MyUser myUser) {
+        this.myUser = myUser;
+    }
+
+    @Override
+    public String toString() {
+        return "PostComment{" +
+                "id=" + id +
+                ", timestamp=" + timestamp +
+//                ", post=" + post +
+                ", comment='" + comment + '\'' +
+                '}';
+    }
 }
